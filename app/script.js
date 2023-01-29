94ee48bc3060c895e98a2b7ea74c87bd61edd535
Natalia S <71900790+NataliaSmola@users.noreply.github.com>
2023-01-29 19:44:03 +0100

    init

---
+++ app/script.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class AppDescription extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>
+        <p>This app will help you track your time and inform you when it's time to rest.</p>
+      </div>
+    );
+  }
+};
+
+class App extends React.Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     status: 'off',
+     time: 1200,
+     timer: null,
+   };
+ };
+
+  formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = Math.floor(time % 60);
+
+    if (minutes < 10) {minutes = "0"+ minutes;}
+    if (seconds < 10) {seconds = "0"+ seconds;}
+    return minutes + ':' + seconds;
+  };
+
+  step = () => {
+    this.setState({
+      time: this.state.time -1,
+    })
+
+    if(this.state.time === 0) {
+      this.setState({
+        status: this.state.status === 'work' ? 'rest' : 'work',
+        time: this.state.status === 'work' ? 20 : 1200,
+      })
+      this.playBell();
+    }
+  };
+
+  startTimer = () => {
+    this.setState({
+      timer: setInterval(this.step, 1000),
+      time: 1200,
+      status: 'work',
+    });
+};
+
+stopTimer = () => {
+  this.setState({
+    timer: clearInterval(this.state.timer),
+    time: 0,
+    status: 'off',
+  });
+};
+
+closeApp = () => {
+  window.close();
+}
+
+playBell = () => {
+  const bell = new Audio('./sounds/bell.wav');
+  bell.play();
+};
+
+
+  render() {
+    const { status, time } = this.state;
+    return (
+      <div>
+        <h1>Protect your eyes</h1>
+        {(status === 'off') && <AppDescription />}
+        {(status === 'work') && <img src="./images/work.png" />}
+        {(status === 'rest') && <img src="./images/rest.png" />}
+        {(status !== 'off') && <div className="timer">
+          {this.formatTime(time)}
+        </div>}
+        {(status === 'off') && <button className="btn" onClick ={this.startTimer}>Start</button>}
+        {(status !== 'off') && <button className="btn" onClick ={this.stopTimer}>Stop</button>}
+        <button className="btn btn-close" onClick={this.closeApp}>X</button>
+      </div>
+    )
+  }
+};
+
+render(<App />, document.querySelector('#app'));
